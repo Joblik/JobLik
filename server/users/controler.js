59a78090c50a3ec87bb33ca495d9f536067b233d
Feres Tounsi <113175521+Feres-tounsi@users.noreply.github.com>
2022-12-28 15:58:43 +0100

    authentication done

--- server/users/controler.js
+++ server/users/controler.js
@@ -1,5 +1,7 @@
 
-const { User} = require("./models");
+const User = require("./models");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 
 
@@ -54,6 +56,70 @@ const addUser = async (req, res) => {
     }
   };
 
+  async function login(req, res) {
+    try {
+      const {
+        body: { email, password },
+      } = req;
+  
+      if (!email && !password) {
+        return res.status(301).json({
+          message: "Email or password required",
+        });
+      }
+  
+      const user = await User.findOne({ email });
+  
+      if (!user) {
+        return res.status(400).json({ message: "Bad cred" });
+      }
+  
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+  
+      if (!isPasswordValid) {
+        return res.status(400).json({ message: "Bad cred" });
+      }
+  
+      const token = jwt.sign({ id: user["_id"] }, "SECRETCODE");
+  
+      return res
+        .status(200)
+        .json({ name: user.Username, email: user.email, token, id: user["_id"] });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send(error);
+    }
+  }
+
+  const register = async (req, res) => {
+    try {
+      const {
+        body: { Username, email, password },
+      } = req;
+  
+      if (!email && !Username  && !password) {
+        return res.status(301).json({
+          message: "Please fill all required fields",
+        });
+      }
+  
+      const newpassword = await bcrypt.hash(password, 10);
+  
+      const user = new User({
+        Username,
+        email,
+        password: newpassword,
+      });
+  
+      user.save();
+  
+      return res.status(200).json({ message: "User Saved" });
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  };
+  
+
 
 
   module.exports = {
@@ -61,5 +127,7 @@ const addUser = async (req, res) => {
     UpdateOneUser,
     GetAllUser,
     addUser,
-    DeleteOneUser
+    DeleteOneUser,
+    login,
+    register,
   }
--- server/users/routes.js
+++ server/users/routes.js
@@ -3,7 +3,9 @@ const {
     UpdateOneUser,
     GetAllUser,
     addUser,
-    DeleteOneUser
+    DeleteOneUser,
+    login,
+    register
 
 }= require("./controler");
     
@@ -12,5 +14,7 @@ router.route('/addUser').post(addUser)
 router.route('/DeleteOneUser').delete(DeleteOneUser)
 router.route('/UpdateOneUser').put(UpdateOneUser)
 router.route('/GetAllUser').get(GetAllUser)
+router.route("/login").post(login);
+router.route("/register").post(register);
 
 module.exports = router;
