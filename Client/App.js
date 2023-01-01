f70ac1f7a78070cb9b831d67a1b0cffd31d0d1e2
karim <karimabdelhedi080@gmail.com>
2023-01-01 17:59:34 +0100

    footer component

--- Client/App.js
+++ Client/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 import LoginScreen from './screens/Login/LoginScreen'
+import Footer from './screens/Footer';
 import PostScreen from './screens/onePost/postScreen'
 import dd from './data/dummy'
 
 export default function App() {
   return (
     <View style={styles.container}>
-     <PostScreen data={dd}/> 
-      <StatusBar style="auto" />
+      <View style={styles.view}>
+      <PostScreen data={dd}/>
+      </View>
+    <Footer/> 
     </View>
   );
 }
@@ -18,6 +21,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
