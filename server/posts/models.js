4a4a9aee33292d3ee8fd8ec5a1b4716a21f951af
laca <souhaieb_mediouni@hotmail.com>
2023-01-05 14:18:30 +0100

    fixing Posts model/controller and test them win postman/thunder

--- server/posts/models.js
+++ server/posts/models.js
@@ -2,25 +2,30 @@ const mongoose = require('mongoose');
 
 
     const PostsSchema = new mongoose.Schema({
-        Uname: {
-          type: String,
-        },
-        Title: {
-          type: String,
-        },
-        Description: {
-          type: String,
-        },
-        Uimage: {
-          type: String,
-        },
-        Adress: {
-          type: String,
-        },
-        
-        Reward: {
-            type: Number,
-          },
-      });
+
+      Uname: {
+        type: String,
+      },
+      Title: {
+        type: String,
+      },
+      Description: {
+        type: String,
+      },
+      Uimage: {
+        type: String,
+      },
+      Adress: {
+        type: String,
+      },
+
+      Reward: {
+        type: Number,
+      },
+      owner: [{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'User'
+      }]
+    });
     
       module.exports={PostsSchema}
--- server/posts/routes.js
+++ server/posts/routes.js
@@ -1,16 +1,17 @@
 const router = require('express').Router();
 const {
-    UpdateOnePost,
-    GetAllPosts,
-    addPost,
-    DeleteOnePost
-
-}= require("./controler");
+  updatePost,
+  getAllPosts,
+  addPost,
+  deletePost,
+  getOnePost
+} = require("./controller");
     
 
+router.route('/getAllPosts').get(getAllPosts)
+router.route("/:id").get(getOnePost);
 router.route('/addPost').post(addPost)
-router.route('/DeleteOnePost').delete(DeleteOnePost)
-router.route('/UpdateOnePost').put(UpdateOnePost)
-router.route('/GetAllPosts').get(GetAllPosts)
+router.route('/:id').put(updatePost)
+router.route('/:id').delete(deletePost)
 
 module.exports = router;
