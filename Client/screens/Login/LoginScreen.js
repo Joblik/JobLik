68041d7d0db58681703dc931e52e0334ec15d5ab
Feres Tounsi <113175521+Feres-tounsi@users.noreply.github.com>
2023-01-05 19:41:35 +0100

    last update of the forgot password and verification with otp in front

--- Client/screens/Login/LoginScreen.js
+++ Client/screens/Login/LoginScreen.js
@@ -26,13 +26,13 @@ const LoginScreen = ({ navigation }) => {
 
   async function handleSubmit() {
     try {
-      const user = await axios.post("http://192.168.104.6:8080/Users/login", {
+      const user = await axios.post("http://192.168.104.12:8080/Users/login", {
         email,
         password,
       });
 
       if (user) {
-        navigation.navigate("home");
+        navigation.navigate("createpost");
         console.log(user.data.id);
         AsyncStorage.setItem("token", JSON.stringify(user.data.token));
         AsyncStorage.setItem("id", JSON.stringify(user.data.id));
@@ -83,11 +83,6 @@ const LoginScreen = ({ navigation }) => {
         <Button title="connect with Google" />
       </View>
     </SafeAreaView>
-<<<<<<< HEAD
-         
-  
-=======
->>>>>>> 925354979c1d7912cf8c65da85c18c02e92df711
   );
 };
 
