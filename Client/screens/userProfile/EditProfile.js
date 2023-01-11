9bf9da5e73ee8a221ccf6cfafa80da973e6dea80
laca <souhaieb_mediouni@hotmail.com>
2023-01-11 10:36:55 +0100

    userProfile functionality and editProfile

--- Client/screens/userProfile/EditProfile.js
+++ Client/screens/userProfile/EditProfile.js
@@ -1,38 +1,180 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, Image } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, TouchableOpacity, Text } from "react-native";
+import { Image } from "react-native-elements";
+import { ScrollView } from "react-native-gesture-handler";
+import { TextInput } from "react-native-paper";
+import Ionic from "react-native-vector-icons/Ionicons";
 
-const UserProfileScreen = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [profileImage, setProfileImage] = useState('');
-
-  const handleSave = () => {
-    // Save user details to database or storage
-    const user = {
-      name,
-      email,
-      phone,
-      profileImage,
-    };
-    console.log(user);
-
+const EditProfile = ({ navigation, route }) => {
+  const { form } = route.params;
+  console.log("🚀 ~ file: EditProfile.js:10 ~ EditProfile ~ form", form);
+  // const handleChange = (event) => {
+  //   setForm({
+  //     ...data,
+  //     [event.target.name]: event.target.value,
+  //   });
+  // };
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.put(
+        `http://192.168.104.30:8080/Users/UpdateOneUser/${userId}`,
+        data
+      );
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <View>
-      <Text>Name</Text>
-      <TextInput  onChangeText={setName} />
-      <Text>Email</Text>
-      <TextInput value={email} onChangeText={setEmail} />
-      <Text>Phone</Text>
-      <TextInput value={phone} onChangeText={setPhone} />
-      <Text>Profile Image</Text>
-      <Button title="Choose Image" onPress={() => {/* Choose image logic */}} />
-      <Image source={{ uri: profileImage }} style={{ width: 100, height: 100 }} />
-      <Button title="Save" onPress={handleSave} />
+    <View
+      style={{
+        width: "100%",
+        height: "100%",
+        backgroundColor: "white",
+        marginTop: 30,
+      }}
+    >
+      <ScrollView>
+        <View
+          style={{
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
+            padding: 10,
+          }}
+        >
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Ionic name="close-outline" style={{ fontSize: 35 }} />
+          </TouchableOpacity>
+          <Text style={{ fontSize: 16, fontWeight: "bold" }}>Edit Profile</Text>
+          <TouchableOpacity
+            onPress={() => {
+              TostMessage();
+              navigation.goBack();
+            }}
+          >
+            <Ionic
+              name="checkmark"
+              style={{ fontSize: 35, color: "#3493D9" }}
+              onPress={handleSubmit}
+            />
+          </TouchableOpacity>
+        </View>
+        <View style={{ padding: 20, alignItems: "center" }}>
+          <Image
+            defaultSource={form.image}
+            style={{ width: 80, height: 80, borderRadius: 100 }}
+          />
+          <Text
+            style={{
+              color: "#3493D9",
+            }}
+          >
+            Change profile photo
+          </Text>
+        </View>
+        <View style={{ padding: 10 }}>
+          <View>
+            <Text
+              style={{
+                opacity: 0.5,
+              }}
+            >
+              Username
+            </Text>
+            <TextInput
+              placeholder="Username"
+              defaultValue={form.username}
+              onChange={handleChange}
+              style={{
+                fontSize: 16,
+                borderBottomWidth: 1,
+                borderColor: "#CDCDCD",
+              }}
+            />
+          </View>
+          <View style={{ paddingVertical: 10 }}>
+            <Text
+              style={{
+                opacity: 0.5,
+              }}
+            >
+              Email
+            </Text>
+            <TextInput
+              placeholder="Email"
+              defaultValue={form.email}
+              onChange={handleChange}
+              style={{
+                fontSize: 16,
+                borderBottomWidth: 1,
+                borderColor: "#CDCDCD",
+              }}
+            />
+          </View>
+          <View style={{ paddingVertical: 10 }}>
+            <Text
+              style={{
+                opacity: 0.5,
+              }}
+            >
+              Job
+            </Text>
+            <TextInput
+              placeholder="Job"
+              defaultValue={form.job}
+              onChange={handleChange}
+              style={{
+                fontSize: 16,
+                borderBottomWidth: 1,
+                borderColor: "#CDCDCD",
+              }}
+            />
+          </View>
+          <View style={{ paddingVertical: 10 }}>
+            <Text
+              style={{
+                opacity: 0.5,
+              }}
+            >
+              Phone
+            </Text>
+            <TextInput
+              placeholder="Phone"
+              defaultValue={form.phone}
+              onChange={handleChange}
+              style={{
+                fontSize: 16,
+                borderBottomWidth: 1,
+                borderColor: "#CDCDCD",
+              }}
+            />
+          </View>
+          <View style={{ paddingVertical: 10 }}>
+            <Text
+              style={{
+                opacity: 0.5,
+              }}
+            >
+              speciality
+            </Text>
+            <TextInput
+              placeholder="speciality"
+              onChange={handleChange}
+              defaultValue={form.domain}
+              style={{
+                fontSize: 16,
+                borderBottomWidth: 1,
+                borderColor: "#CDCDCD",
+              }}
+            />
+          </View>
+        </View>
+        <View></View>
+      </ScrollView>
     </View>
   );
 };
 
-export default UserProfileScreen;
+export default EditProfile;
